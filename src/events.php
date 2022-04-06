08db2cd201221bbaf5ffa35bcb43148fa875a609
v.korobov <v.korobov@corp.mail.ru>
2022-04-07 02:31:12 +0300

    Add events.php

---
+++ src/events.php
@@ -0,0 +1,13 @@
+<?php
+
+enum EventsType: string
+{
+    case NEW_MESSAGE = "newMessage";
+    case EDITED_MESSAGE = "editedMessage";
+    case DELETED_MESSAGE = "deletedMessage";
+    case PINNED_MESSAGE = "pinnedMessage";
+    case UNPINNED_MESSAGE = "unpinnedMessage";
+    case NEW_CHAT_MEMBER = "newChatMembers";
+    case LEFT_CHAT_MEMBER = "leftChatMembers";
+    case CALLBACK_QUERY = "callbackQuery";
+}
