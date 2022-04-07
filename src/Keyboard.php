fa1795ba6bdf6c04d50de6d19fee5032bc28fdb0
v.korobov <v.korobov@corp.mail.ru>
2022-04-08 02:50:27 +0300

    Refactoring: added docs to Keyboard class

--- src/Keyboard.php
+++ src/Keyboard.php
@@ -4,7 +4,10 @@ namespace Dasshit\IcqBot {
 
     use JsonSerializable;
 
-
+    /**
+     * class Keyboard
+     * @package Dasshit\IcqBot
+     */
     class Keyboard implements JsonSerializable
     {
         public array $rows;
@@ -14,16 +17,27 @@ namespace Dasshit\IcqBot {
             $this->rows = [];
         }
 
+        /**
+         * @param array $row Массив кнопок в клавиатуре
+         * @return void
+         */
         public function addRow(array $row): void
         {
             $this->rows[] = $row;
         }
 
+        /**
+         * @param Button $button Кнопка для клавиатуры
+         * @return void
+         */
         public function addButton(Button $button): void
         {
             $this->rows[] = [$button];
         }
 
+        /**
+         * @return array
+         */
         public function jsonSerialize(): array
         {
             return $this->rows;
